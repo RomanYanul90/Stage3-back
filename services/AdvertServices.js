3744e2da32a2c68e89f781bb1574d612c1056bdc
Roman <roman@inspr.com>
2021-01-22 16:23:51 +0300

    Minor changes

--- services/AdvertServices.js
+++ services/AdvertServices.js
@@ -9,29 +9,29 @@ export const createAdvertService = async (title, description, category, price, c
 };
 
 export const getAllAdvertsService = async () => {
-    return await Advert.find();
+    return Advert.find();
 };
 
 export const getAdvertByIdService = async (id) => {
-    return await Advert.findById(id);
+    return Advert.findById(id);
 };
 
 export const getCurrentUserAdvertsService = async (ownerId) => {
-    return await Advert.find({ownerId: ownerId});
+    return Advert.find({ownerId: ownerId});
 };
 
 export const getAdvertsByOwnerNameService = async (owner) => {
-    return await Advert.find({ownerUserName: owner});
+    return Advert.find({ownerUserName: owner});
 };
 
 export const findAdvertByTitleService = async (title) => {
-    return await Advert.find({title: title});
+    return Advert.find({title: title});
 };
 
 export const editAdvertService = async (id, updates) => {
-    return await Advert.findByIdAndUpdate(id, updates);
+    return Advert.findByIdAndUpdate(id, updates);
 };
 
 export const deleteAdvertService = async (id) => {
-    return await Advert.findByIdAndDelete(id);
+    return Advert.findByIdAndDelete(id);
 };
